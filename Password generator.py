cc41a8f50790ef8a9f6f7882b105a4a9fe0808ea
AyushaJadhav <130702125+AyushaJadhav@users.noreply.github.com>
2024-05-30 21:04:21 +0530

    Update Password generator.py

--- Password generator.py
+++ Password generator.py
@@ -1,21 +1,36 @@
-import string
+from tkinter import *
 import random
+import pyperclip
 
-if __name__ == "__main__":
-    s1 = string.ascii_lowercase
-   
-    s2 = string.ascii_uppercase
-   
-    s3 = string.digits
- 
-    s4 = string.punctuation
- 
-    plen = int(input("Enter password length\n"))
-    s = []
-    s.extend(list(s1))
-    s.extend(list(s2))
-    s.extend(list(s3))
-    s.extend(list(s4))
-    print("Your password is: ")
-    print("".join(random.sample(s, plen)))
- 
+
+tk=Tk()
+tk.geometry('400x400')
+tk.configure(background='light blue')
+
+pswd=StringVar()
+passlen=IntVar()
+passlen.set('Enter Length')
+
+def password_generator():
+    characters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 !@#$%^&*()'
+    password=''
+    if passlen.get()>=8:
+        for i in range(passlen.get()):
+            password+=random.choice(characters)
+        pswd.set(password)
+
+def copyclipboard():
+    random_password = pswd.get()
+    pyperclip.copy(random_password)
+    Label(tk,text="Copied to Clipboard",bg="light green").pack(pady=6)
+    
+Label(tk, text="Enter the number to get password \n (Minimum length should be 8)",bg='grey',fg='white').pack(pady=7)
+
+Entry(tk, textvariable=passlen).pack(pady=7)
+
+Button(tk, text="Generate Password", command=password_generator,bg='black',fg='white').pack(pady=12)
+Entry(tk, textvariable=pswd).pack(pady=7)
+
+Button(tk, text="Copy to clipboard", command=copyclipboard,bg='black',fg='white').pack()
+
+tk.mainloop()
